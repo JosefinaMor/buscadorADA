8704b3ed4d2db377d14a35ea130152c09c1363b6
JosefinaMor <josefina.morales.1765@gmail.com>
2022-01-15 19:17:11 -0300

    i began with de paging function

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-fetch('https://ghibliapi.herokuapp.com/people/598f7048-74ff-41e0-92ef-87dc1ad980a9')
+const moviePaging = document.getElementById("movie-paging");
+
+fetch('https://ghibliapi.herokuapp.com/films')
 .then(res => res.json())
 .then((data)=>{
     console.log(data);
+    var i = 0;
+    const cardsHTML = data.reduce((acc, element) =>{
+        if (i < 4){
+            acc = acc +`<div class="card">
+                            <img src="${element.image}">
+                            <h4>${element.original_title_romanised}</h4>
+                            <span>${element.original_title}</span>
+                        </div>` 
+            i++;
+        } 
+        return acc;   
+    },"");
+    moviePaging.innerHTML = cardsHTML;
 })
